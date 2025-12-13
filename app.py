7e2c72bf151dec99a4de7e2eed0885b97dc4ef5e
Rohan-651 <22f2001391@ds.study.iitm.ac.in>
2025-12-13 09:11:11 +0530

    Enable Gradio queue to expose API endpoint

--- app.py
+++ app.py
@@ -39,5 +39,7 @@ with gr.Blocks() as demo:
     outputs=[output],
     api_name="solve")
 
+demo.queue()
+
 if __name__ == "__main__":
     demo.launch(server_name="0.0.0.0", server_port=7860, share=False)
